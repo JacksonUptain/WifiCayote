d4f8a8a60dc2585a8b9c39e132663ebd7c73c072
Jackson Uptain <111402072+JacksonUptain@users.noreply.github.com>
2023-11-04 11:36:28 -0400

    Add files via upload

---
+++ CayoteWifi.sh
@@ -0,0 +1,165 @@
+#! /usr/bin/bash
+
+echo -e "\e[1;34m                                                                                                                                                      
+                                                                                                                                                      
+                                                                                                                                                      
+                                                                                                                                                      
+                                                                                                                                                      
+                                                                                                                                                      
+                                                                                                                                                      
+         .                       ..                                                                                     ..                  .,.       
+       .;:;.                    .lo.                                .;;lddodxOOOOOkkxl:::;;.            ..            .,;;'.               'cc:.      
+       .'''.        .'.        'c::c'                          .;lxkXWWMWWWWWWWWWMWWWWWWWWN0xxl,...    .:o;.           .''.               .::,:;      
+                   .lxl.         ,,          .,            .coxKNNWWWWNNWWWWWNNNNWWMWWWWWNXXNNXX00k:.   ..                                  .,.       
+                    .;.                     .:c,.       ,okKWWWWWWWWWNNNWWWWNXNNNNWWWWNNWNNXNNNWNNWN0kl,                                              
+                                             .'.     .;dXWWWWWWWWNNNXKXWWWWMWWWWWWWWWWWWWMWWWWWWWWWMWWWNk:.                                           
+                                                   .oKNNWWWNNXXKKKKKK00KNWWWWWWNK0KKXXNMWNNNNWWWWWWWMWWWWWKo.                                         
+                                                 .:ONNNNWWNXK000OOOKKOO0KNWWWWWX0000KKXNXXXXXNWWWWWWWMMMMWWWKl.                                  ..   
+   .,;;.                                        ,kNWNXNNNXK0000000O00O0KXNWWWWNXKK0O0000KXXXNNWWWWWWWWMMWWWWWWO;                               .':c;. 
+  'dxkkkc.             ..                     .cKWNNNNNX0000KKKK000000KXNWWMWNNXKKK000KK000KXNNNWWWWWWWWNXXXXXNXo.                   ..         .''.  
+ ,ddclclko.           ':::.          .       .xNWNXXXXX0000KKKKKKKKKKXXXXNWWNXXK0KXXNWWWWXKKKKXNNWWWWWNNKOOOO0KNWO,                .cxo'.             
+ cololocox,           ..'..        .:xl.    'kWWNXKKKK000KKKXKKKKKKKXNNNWWWNNKXXXXNWWXx0MWNKKKKXXXXXNWWNKOOOOO0XWWK:               .,l:.              
+ cololocox,                        .ckl.   'ONXXK0KKK0OO0K0KK0KKKKKKKXNNNNNXX0KXNNWW0;.,0WMNKKK0000KKXNWNK00O0KNWWWNo.      ...                       
+ cololocox,                          .     cXK000O0000000K0KXK0KKNXKKXXKK00000KNWMWk'   lWWX0000OOOOO0KNNNNXKNNNNWWWX:     .;c:.                      
+ cololocox,                               'kK0O00OOO0KKKXXXXWWNXNWNXK0OOO0K0KXNWWMO'   .kWWKKXK0O0OO0000KKNNNWXXWWWWWO,     .'.                       
+ cololocox,                               dNK00KK00O0KKXNNNWWNWWWNX00OO0KXXNNWWNXk;    ,O0l.lNWXK0000KKK00KKKXNWNNNNNWd                           ';  
+ cololocox,                              :KK00KKKK000KKKKKXNWWWWWNKKKKKXNNWMMKo,..      .. .xWWNK0000KXX0OOOO00XXKXNNWXc                        .;cc:.
+ cololocox,                 .,.          dX0O00000KKXXXNNXNWWWWWNNXKXXNWNWMMWl           .lKWMWNXXXXXKK00OOOOOO0KXNNWWWx.                       .;:;;'
+ cololocox,                ,cc:'         dXOOOO000KKXNNNNNNNNNNXKXK0KXWWMWKkl.           :NWWWNXXNWWNXKKK000O00KKXWMWWWx.              .'         ..  
+ cololocox,               .::;:;.        dXO000000KXXXXNXXKXXKXXXKKKNWWXx:.             'OWWWNXXXNNNNNXXXKKK00KKKXNWWWWd.             'od;.           
+ cololocox,                 .'           dXO0K0OO00XXKXXXK0000KXXXNWKd:.                :NWWWWNXXNNNXXKXXKKKK00KKNNWNNWd               ',.            
+ :ololocox,                              dXO0KK0O0KXNXXXK00O00KXNWMNk:.....             :XMMMWWWXXXXXXXXXXXXK000KXNWWWWd                              
+ :olololox,                              dX0K00OO00KXKXK0O00KKKKXNWWWWXKKk,             .oWWMMWWNXKKXXKKKXXXXKKKXXNWWWWd.                             
+ :ololocox,    .','.                     dXKK0O0O0O0KK00OO0KKKKKKKXWMWWW0'              'kWMMWWWXXKKXK00KXKXNNWWWNNWWWWd.                         ..  
+ :ololocox,   .dxkkx;                    ,ONX0O0000KKKK00O0K00KKKXNWMWWO,               .dWMWWWWNNWNXXK0O0KXXXNWWWWWWW0,        .'.              'cc;.
+ :olololox,  .cd:clox;.        '.         dWXKKKK0KKXNXK00KK00KXXNWWWNx.                .xWWWWWWWWWWWNXKXXNNWWNXXNWMWWd        .;c:.             ..'. 
+ :ololocox,  ,oolllldd'      .,c:.        ;0NNXXKKXXXXNXXNNNNXKXNWWWXo.                 'OWWWWWWWWWWWWWWWWWWWWWWWWWWWK;          ..                   
+ :ololocox,  ,oollllxd'        ..          ;KWWNNXXXNNNNNNWWNNNWMNk:'.                 .cKWWWWWWWWWNNWWWWWWWWWWWWMWWNl                       ':.      
+ cololocox,  ,oolllldd,                     :KWWWWNXXNXXXNWWWWOol;                     .cKWWWMWWWWWWWWWWWWWWWWMMWMWNd.                     .,cc:.     
+:lllololox,  ,oollllxd,                      ;0WWWWWNXXNWWN0xl.                        'OMWWMMMWWWWWWWWWWWWMMMWMMWXl.                      .::,:,     
+xolloldcox,  ,oollllxd,                       ,OWWWWWWWKxc,.                          .lXMMWMMMWWMWWWWWWWWMWWMMMWXc                          .'       
+xolloldcox,  ,oollllxd,                       .oNWNXOl:.                             .;KMWMMMMMWWWWWWWWWWWWMMWWWO;                                    
+xollolocox,  ,oollllxd'                   .;:l0XKkl'.                                .xMWWMMMMMMWWWWWWWWMMWMMWXd.                                     
+xlllololox,  ,oolllldd'                 .:ONKkc'.                                   .oXWMMMMMMMMMMMWWWWMMWWWWk,                                       
+xollolocox,  ,oolllldd'               ,kX0d;.                                       ;KWWMMMMMMMMMMWWWWMMWWWKc.           ..                           
+xoooolocox,  ,oolllldd'             .oXXd'                                        .lXWWMMMMMMMMMMMMMMMWWWKd.            ;o;             .:olc:lc.     
+,lololocox,  ,oolllldd'            ;ONk,                                         'xNWWWMWMMMMMMMMWWMNOkkl.              ':'           .;k0xl:..lxo'   
+'lololocox,  ,oollllxd,          .dX0:.                                        'kXWWMMMWWWWWWWWWWWXx,                              .:lkOOOo;'...'odoo:
+kkllololc:' ,kxoododl,.         .kNk'                                        .c0NWWWMMWMMWNK00dccc,                             .:d0WKdddo;....,,..:xO
+'oooolc'.'coxxdddx0O,           lN0,                              .cxd;      .kWX0KKOdoooo;...                               .,d00Oxx:;l. .;,.  'o;..'
+ cooooc'..;ldddk0Kx'           .xMd                        ,c;';oldXMMO.    .oXK:...                                .:c::clllkd;,....;;.  ..     .::. 
+ coloo:.:lodoxko:,.            cX0; .;.                  .oXWNXNN0l:kWO.    '0M0,                                  ;kk:,lOKXX0;..  .;,.            ';:
+ :oloo:.:Ol....               :KXl. 'd,                  '0WKdc;,.  lWO.    ,KM0'                               .;oo,.  ;occdo,,,..;.                .
+ :ooolocox,                 .dX0;   cKx.                 ,0Wl       lWO.    ,KWx.                             .:oxd::lc;'. .:' ....                   
+ cololocox,               .c0Nx.  .dNMN:                 ,0Nc       :XX:    ,KWo                             ;xkl,..;;.   ,,.                         
+ cololocox,             .:0NKo.  .kWWWWk.                ,0N:       .xM0'   ,KWd'. .'.                     .oK0xdo,.                ..,lcl:..         
+oxololocox,            :ONNd'.';oKKdoKM0'               .oNx.       .dMK,   ,KW0Okoldx:.               .:dd0XK0kc.                 'loOX00kxo,   .    
+:Oolololoko.          :XNKxclx0XXO;  :KNo       .oo.   'OWk.    .,;clkWK,   ,KWX0KK00Ox:.           ,::coxOkl:''.     ,,::'l:;.    'oOOxldO0d'.'cdl,. 
+.coooldloOxloc.       :XNXXKKOl,..   ;KMx.     .xNk.  .kW0, .',c0K0N0OWK;   .oN0;,;;kNXOk;..   .,,coo:,:;''.         .;l00OK0d:.   'dXk'..'xk,c0Oll0O,
+xxolololoOo.,oxl;.     ,cc;..       'ON0,    .lKNk'  :KNk, .,;dOxkKx'.c:.     ;;   ';lOkOOkd;:xXKd:..;:.             .c0OocoKKc.   ,oKd....lxdK0:.,kO,
+'cololdloOo'cooooc;cc,,.            ;XK;   .cONWO'  ;XWx. .locxO;.,.              ..  ;OX000KWNk:...',           ..  'x0; ..oXl.  ,dx0x... .:dXl...lk:
+.cololocoOl .,cdo,..,;cl;;c;        ;X0'  .dNXNNc   :NK; .;kKko:'                     'l:..dXNKl.             .:lkkl:,d0:. .xXo;,'o0dx0l... .ok'.ldoc'
+;xololdloOl.  .,clc;;;;:,..:::;'.   ;X0,  oN0::0O,  :NK;;OOOOdc.                       '::lkdo00,             :OOOXXxoxk:.. oXd:oOOo;oK0c... ...dO;   
+ cololdlok;   .;dxl;:c'.,,.  ':cddc,lX0' .kMO. cXd. :NKxxkOOxlc:.                     ':,...,d0Kxl'.          ;Kk,lKOk0x' ..dXc,d0Kc.oXXo..    lOl.   
+ldolololoOl'..;;'xKOOOxc:ol;.   .:dx0W0' .kMk,':0d. cNWNOc;..''..                     ..    .lxkK0xo'         .ko..l00kKd. .:kddKKOllKKk:.    ,Oo.    
+ddllolocoOo. .dKO0OkKKXx'. .c,  ..  ,00' .kWX0KNWd. .cdl..;.                                 .;odON0, .       'dk;..o0k00;...:kOOO0Od0d.     :xc.     
+,..;;....',..cO0kkdokkXx;;;;:c:::::;c00' .lNWkcxd.                                        .   ...;Oxc:::;::::::cxxc;cxkkOo' .;cllcc:l0o.  .:lxd:::::::
+,      ':::cc:lc.;;..;kxlllll,.;llcclkd.   ;o, .                                              .:'.,llllcclllllllcclc,..:d0k,...  . .:;. .;ddllllllllll
+   .;;;:ccc,.. .,. .,:ccccccc:;:cc:c,.                                                         .;;:ccccccccccccccccc:'  'cl,       ..  ,dolccccccccccc
+ ,::ccccc;. .,::c:;:ccccccccccccc,.                                                           ':,..,ccccccccccccccccc::;. ...      .. .ooccccccccccccc
+;;;;;;;;;;;,;;;:;;;;;:;;;;;;;::;:'  .'. .'  .'.  ..   '. ... ... .'  .. ... .;,.  ..  '. .. .,;;;;;;;;;;;;;;;;;;;;;;:;;:;. ...      .,;;;;;;;;;;;;;;;;
+                                                                                                                                                      
+                                                                                                                                                     \e[0m"
+echo -e "\e[1;34m------------------------------------------------------------------------------------------------------------------------------------------------------ \e[0m"
+echo -e "\e[1;34m 
+ ██████╗ █████╗ ██╗   ██╗ ██████╗ ████████╗███████╗
+██╔════╝██╔══██╗╚██╗ ██╔╝██╔═══██╗╚══██╔══╝██╔════╝
+██║     ███████║ ╚████╔╝ ██║   ██║   ██║   █████╗  
+██║     ██╔══██║  ╚██╔╝  ██║   ██║   ██║   ██╔══╝  
+╚██████╗██║  ██║   ██║   ╚██████╔╝   ██║   ███████╗
+ ╚═════╝╚═╝  ╚═╝   ╚═╝    ╚═════╝    ╚═╝   ╚══════╝
+                                                   
+██╗    ██╗██╗      ███████╗██╗                     
+██║    ██║██║      ██╔════╝██║                     
+██║ █╗ ██║██║█████╗█████╗  ██║                     
+██║███╗██║██║╚════╝██╔══╝  ██║                     
+╚███╔███╔╝██║      ██║     ██║                     
+ ╚══╝╚══╝ ╚═╝      ╚═╝     ╚═╝                     \e[0m"
+echo -e "\e[1;34m---Linux Wi-Fi Hacking Tool. Developed By: Jackson U-------------------------------------------------------------------------------------------------- \e[0m"
+alias Cayote='bash CayoteWifi.sh'
+if [ -x "$(command -v git)" ]; then
+  echo -e "\e[1;36m  [\xE2\x9C\x93] Git is Installed and Ready \e[0m"
+else
+  echo -e "\e[1;31m  [\xE2\x9C\x97] Git is not installed yet...\e[0m"
+  sudo apt-get install git
+fi
+
+if [ -f "ArduinoIDE.AppImage" ]; then
+  echo -e "\e[1;36m  [\xE2\x9C\x93] Arduino IDE is Installed...\e[0m"
+  chmod +x 'ArduinoIDE.AppImage';
+else
+  wget -O ArduinoIDE.tar.xz "https://github.com/arduino/Arduino/releases/download/1.8.19/arduino-1.8.19.tar.xz";
+  tar -xf ArduinoIDE.tar.xz;
+  mv arduino-1.8.19 ArduinoIDE;
+  rm -rf ArduinoIDE.tar.xz;
+  sh 'ArduinoIDE/build/linux/dist/install.sh';
+  echo -e "\e[1;36m  [\xE2\x9C\x97] ArduinoIDE installed...opening \e[0m";
+  wget -O ArduinoIDE.AppImage 'https://github.com/arduino/arduino-ide/releases/download/2.2.1/arduino-ide_2.2.1_Linux_64bit.AppImage';
+fi
+
+
+
+
+echo -e "\e[1;33m  [\xE2\x9C\x93] IP ADDRESS: $(ip addr show wlan0 | grep inet | awk '{print $2}' | cut -d/ -f1) \e[0m"
+
+
+echo -e "\e[1;31m
+  _                ___                                      
+ |_)  _.  _ o  _    |  ._   _ _|_ ._     _ _|_ o  _  ._   _ 
+ |_) (_| _> | (_   _|_ | | _>  |_ | |_| (_  |_ | (_) | | _> 
+                                                              \e[0m"
+echo -e "\e[1;35m \n1.  Start Local Host/Website (Using CayoteWifi) \n2.  Flash Firmware/.ino file to ESP8266 \n3.  Unplug/Replug the ESP8266 \n4.  Connect to 'Wifi' network using the current device \n5.  Start/Restart CayoteWifi \n6.  Start/Restart Local Host/Website \n7.  Reflash Firmware/ .ino file to the ESP8266 \n8.  READ CAREFULLY(Special instructions below) \e[0m"
+
+echo -e "\e[1:36m  [\xE2\x9C\x97] INSTRUCTIONS FOR ESP8266: When you open the ArduinoIDE you must first click on File > Open File. Select the .ino file containing your code(IF you have one made already).
+  [\xE2\x9C\x97] When you have selected your file go to...File > Preferences > Additional Boards URL. Copy & Paste: https://arduino.esp8266.com/stable/package_esp8266com_index.json 
+  [\xE2\x9C\x97] Next head to...Tools > Boards > Boards Manager. Once inside search for esp8266 and Download the Latest Version. Click Verify & Upload [\xE2\x9C\x93]
+     NOTE: On Line 15 of the .ino file, change the IP address to be your IP as is listed above - AFTER -  you have flashed and connected to it via WiFi the first time. \e[0m"
+
+echo " "
+
+echo -e "\e[1:36m  [\xE2\x9C\x97] INSTRUCTIONS FOR LOCAL HOSTING WEBSITE: I locally hosted a website due to the lack of storage on the esp8266. Feel free to do what you like with it, but if you would like the default RickRoll just select YES.
+ The idea is that when you connect to your flashed ESP8266, the ESP will chnage your IP. Once you run this tool, the IP will be different. You can then change Line 15 of the .ino to be 'That' IP. THANKS!
+  Otherwise: 'cd /var/www/html/' and change the html file/website as you see fit, then 'sudo service apache2 start'.  
+ I do not encourage any illegal behavior w/this device.\e[0m"
+
+read -p  "DO YOU WANT TO LOCAL HOST THE DEFAULT RICKROLL ON CONNECTED WI-FI? (YES/no) >> " yn2
+
+
+if [ "$yn2" = "YES" ]; then
+    sudo git clone "https://github.com/JacksonUptain/WifiRickroll-CayoteWifi-.git"
+    sudo rm -rf /var/www/html/*;
+    sudo cp -R  WifiRickroll-CayoteWifi-/.  /var/www/html/;
+    sudo service apache2 status;
+    sudo service apache2 stop;
+    sudo service apache2 start;
+else
+    echo "Exiting..."
+fi
+
+
+echo " "
+
+read -p  "Plug in ESP8266 : Do you want to open ArduinoIDE to Flash your ESP8266? (YES/no) >> " yn
+
+
+if [ "$yn" = "YES" ]; then
+    echo -e  "\e[1;36m Opening ArduinoIDE... \e[0m"
+    chmod +x 'ArduinoIDE.AppImage';
+    ./ArduinoIDE.AppImage;
+else
+    echo "Exiting..."
+fi
+
+
+echo " "
+echo -e "\e[1;35m Restart CayoteWifi if exit is Unexpected... \e[0m"
